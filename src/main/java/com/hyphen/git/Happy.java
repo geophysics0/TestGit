ab1a8f7ff267cb595a862e98a3c642a42c2f6143
ecplise-user <ecplise_user@hyphen.com>
2020-07-10 03:46:58 +0800

    test conflict right git

--- src/main/java/com/hyphen/git/Happy.java
+++ src/main/java/com/hyphen/git/Happy.java
@@ -2,7 +2,7 @@ package com.hyphen.git;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Test9999999999999999");
+		System.out.println("Test9999999999999999~~~right");
 	}
 
 }
