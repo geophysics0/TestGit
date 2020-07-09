c94799b8f9cf9a05acd9928ab59a7fd88c0c1108
ecplise-user <ecplise_user@hyphen.com>
2020-07-10 04:12:07 +0800

    create new branch hot_fix

--- src/main/java/com/hyphen/git/Happy.java
+++ src/main/java/com/hyphen/git/Happy.java
@@ -3,6 +3,7 @@ package com.hyphen.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Test9999999999999999~~~right");
+		System.out.println("hot_fix");
 	}
 
 }
